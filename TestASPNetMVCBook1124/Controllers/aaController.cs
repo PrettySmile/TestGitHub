c284f6d5383e90fd39ba63ee834f5d65983c7479
許淑婷 <ns00016226@hotmail.com>
2019-10-28 14:19:06 +0800

    add aaController file

---
+++ TestASPNetMVCBook1124/Controllers/aaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestASPNetMVCBook1124.Controllers
+{
+    public class aaController : Controller
+    {
+        // GET: aa
+        public ActionResult Index()
+        {
+            dfdsfdsfa
+            return View();
+        }
+    }
+}
